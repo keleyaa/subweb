b33862ddc8be223081bdae1bb1065b2bfa71c6f5
keleyaa <miaomiao59420@gmail.com>
2026-09-20 02:33:37 +0800

    style: normalize deploy path resolution

--- scripts/docker-deploy.sh
+++ scripts/docker-deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -eu
 
-SCRIPT_DIRECTORY=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
-PROJECT_DIRECTORY=$(CDPATH= cd -- "$SCRIPT_DIRECTORY/.." && pwd)
+SCRIPT_DIRECTORY=$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd)
+PROJECT_DIRECTORY=$(CDPATH='' cd -- "$SCRIPT_DIRECTORY/.." && pwd)
 . "$SCRIPT_DIRECTORY/lib/release-image.sh"
 
 fail() {
